13713a4f5c6be2035ef0bb73f5d35c33a945315b
eddierosenbaum <49243589+eddierosenbaum@users.noreply.github.com>
2019-05-16 15:26:38 -0400

    no clinic

--- js/scripts.js
+++ js/scripts.js
@@ -6,58 +6,7 @@ const map = new mapboxgl.Map({
   zoom: 4.9
 });
 
-var clinics= [
-  {
-    lng: 38.5137507,
-    lat: 48.788572,
-    name: 'Toshkivka'
-  },
-  {
-    lng: 39.65141,
-    lat: 48.7699,
-    name: 'Stepove'
-  },
-  {
-    lng: 38.5561875,
-    lat: 48.7221869,
-    name: 'Orikhove'
-  },
-  {
-    lng: 38.4287579,
-    lat: 48.7523564,
-    name: 'Novoivanivka'
-  },
-  {
-    lng: 38.2952848,
-    lat: 48.6928385,
-    name: 'Zolote'
-  },
-  {
-    lng: 38.2905473,
-    lat: 48.6450918,
-    name: 'Druzhba'
-  },
-  {
-    lng: 39.0904825,
-    lat: 48.7685625,
-    name: 'Heivka'
-  },
-  {
-    lng: 38.4598932,
-    lat: 48.7517199,
-    name: 'Hirske'
-  },
-  {
-    lng: 39.6234141,
-    lat: 48.7193748,
-    name: 'Komyshne'
-  },
-  {
-    lng: 39.6234141,
-    lat: 48.7193748,
-    name: 'Komyshne'
-  },
-];
+
 console.log(clinics)
 clinics.forEach(function(clinic) {
 
